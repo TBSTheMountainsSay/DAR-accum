91a2d6090fb6f26c6138bb72abdf0c7d98038d49
RogotnevIvan <ivan.rogotnev@cgtribe.com>
2025-10-03 14:56:08 +0400

    preparing for develop

--- DAR-accum/src/App.tsx
+++ DAR-accum/src/App.tsx
@@ -1,7 +1,12 @@
 import "./assets/styles/reset.scss"
 import "./assets/styles/global.scss"
+import {useEffect} from "react";
 
 function App() {
+
+  useEffect(() => {
+    document.documentElement.setAttribute("color-scheme", "dark")
+  }, [])
 
   return (
     <div>
