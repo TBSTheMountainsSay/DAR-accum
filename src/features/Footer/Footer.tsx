54f6efa7380fde815c1d7327d24bbd5c0e2b0f20
RogotnevIvan <ivan.rogotnev@cgtribe.com>
2025-10-20 13:26:08 +0400

    Обновил футер

--- src/features/Footer/Footer.tsx
+++ src/features/Footer/Footer.tsx
@@ -15,16 +15,34 @@ const Footer = () => {
     return (
         <div className={styles.wrapper}>
             <div className={styles.content}>
-                <div className={styles.logoWrapper}>
-                    <img src={logo as string} alt={"Logo"} className={styles.image}/>
-                    <CustomTitle text={"С уважением к Вам и Вашему делу!"} type={"text"}/>
+                <div className={styles.leftBar}>
+                    <CustomTitle text={"Наши партнёры:"} type={"title"}/>
+                    <div className={styles.partnersWrapper}>
+                        <CustomTitle text={"• Сеть магазинов Лемана Про"} type={"text"}/>
+                        <CustomTitle text={"• Группа компаний РОСТ"} type={"text"}/>
+                        <CustomTitle text={"• Сеть магазинов Добрострой"} type={"text"}/>
+                        <CustomTitle text={"• АО птицефабрика Краснодонская"} type={"text"}/>
+                        <CustomTitle text={"• Волгоградский керамический завод"} type={"text"}/>
+                        <CustomTitle text={"• ООО Арбитр"} type={"text"}/>
+                        <CustomTitle text={"• ООО Агрофарт"} type={"text"}/>
+                        <CustomTitle text={"• ООО Агросинтез"} type={"text"}/>
+                        <CustomTitle text={"• ООО Агрофреш"} type={"text"}/>
+                    </div>
                 </div>
                 <div className={styles.contacts}>
+                    <CustomTitle text={"Наши контакты:"} type={"title"}/>
                     <CustomTitle text={"И.П. Домрачева Диана Сергеевна"} type={"text"}/>
                     <CustomTitle text={"Город-Герой Волгоград: 8 (982) 884-51-21"} type={"text"} onClick={() => handleCopy("8 (982) 884-51-21")}/>
                     <CustomTitle text={"Город-Герой Новороссийск: 8 (912) 012-72-90"} type={"text"} onClick={() => handleCopy("8 (912) 012-72-90")}/>
                     <CustomTitle text={"DAR_ServisAKB@mail.ru"} type={"text"} onClick={() => handleCopy("DAR_ServisAKB@mail.ru")}/>
                 </div>
+            </div>
+            <div className={styles.copyright}>
+                <div className={styles.logoWrapper}>
+                    <img src={logo as string} alt={"Logo"} className={styles.image}/>
+                    <CustomTitle text={"С уважением к Вам и Вашему делу!"} type={"text"}/>
+                </div>
+                <CustomTitle text={"© 2023-2025 ИП Домрачева Д.С. | Компания «ДАР»"} type={"text"}/>
             </div>
         </div>
     );
