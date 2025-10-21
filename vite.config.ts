7eceb5838e37d0ded757de4792783c0379b1b96d
RogotnevIvan <ivan.rogotnev@cgtribe.com>
2025-10-22 01:37:26 +0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,11 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/DAR-accum/',
   plugins: [react()],
   resolve: {
     alias: {
       'src': path.resolve(__dirname, './src'),
-      '@': path.resolve(__dirname, './src'), // можно и @
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
